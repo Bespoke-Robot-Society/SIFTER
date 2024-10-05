b8a6cbb3880979250f55a04c51cc7fc081453ecf
Andy Ponce <andy.ponce@outlook.com>
2024-10-05 19:00:26 -0400

    Created profile component, created placeholder about page

--- server/pages/about-us.py
+++ server/pages/about-us.py
@@ -1,10 +1,11 @@
 import dash
 from dash import html, dcc, callback, Input, Output
+from components.profile import Profile
 import dash_bootstrap_components as dbc
 dash.register_page(__name__)
 
 styles = {"padding": "20px"}
-profile_desc_ex = \
+description_placeholder = \
 """
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus tempus vestibulum. 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
@@ -16,39 +17,32 @@ Nunc nibh libero, iaculis sit amet vehicula in, auctor nec ante. Mauris imperdie
 Curabitur malesuada pellentesque rutrum. 
 Proin auctor ligula sem, et tristique magna iaculis mollis. Nulla ac dui justo. 
 """
-single_profile_ex = \
-                dbc.Row(
-            [
-                dbc.Col(
-                    html.Img(
-                        src="https://via.placeholder.com/200",  # replace with your image URL
-                        alt="Profile Picture",
-                        className="img-fluid rounded-circle",
-                        style={"width": "200px", "height": "200px"}
-                    ),
-                    width={"size": 2, "offset": 1},
-                    className="text-center"
-                ),
+image_placeholder = "https://via.placeholder.com/200"
+bespoke_robot_image = \
                 dbc.Col(
                     [
-                        html.H3("Your Name"),
-                        html.P(profile_desc_ex),
-                        html.P("Based in: City, Country"),
+                    html.Img(
+                        src="/assets/brs_transparency.png",  # replace with your image URL
+                        alt="Profile Picture",
+                        style={"width": "300px", "height": "300px"}
+                    ),
                     ],
-                    width=7
-                ),
-            ],
-            className="mt-4"
-        )
+                    width={"size": 2, "offset": 5},
+                    className="text-center"
+                )
+andy_profile = Profile("Andy Ponce", description_placeholder, "/assets/AndyPhoto.JPG")
+profile_placeholder = Profile("Your Name", description_placeholder, image_placeholder)
+
 layout = html.Div(
     [
         html.H1("About Us"),
-        single_profile_ex,
-        single_profile_ex,
-        single_profile_ex,
-        single_profile_ex,
-        single_profile_ex,
-        single_profile_ex,
+        bespoke_robot_image,
+        profile_placeholder,
+        andy_profile,
+        profile_placeholder,
+        profile_placeholder,
+        profile_placeholder,
+        profile_placeholder,
     ],
     style=styles,
 )
