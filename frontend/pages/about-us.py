be4b0691b51dd26bb5ad3fd78d0756b5ebcc5382
John McCardle <mccardle.john@gmail.com>
2024-10-06 20:57:20 -0400

    About Us page (#12)
    * Do I want to code something to make the BRS logo keep its aspect ratio? Not really, I think I'll just make the image itself square
    
    * Remove padding from image (which also distorted it)
    
    * Profile component update: Create multiple <p> tags for bios
    
    * Aaron's Bio
    
    Co-authored-by: John McCardle <mccardle.john@gmail.com>
    
    * John's bio
    
    * Markdown bios
    
    * Andy's bio
    
    Co-authored-by: John McCardle <mccardle.john@gmail.com>
    
    * Rob's bio
    
    Co-authored-by: John McCardle <mccardle.john@gmail.com>
    
    * Jason's bio
    
    Co-authored-by: John McCardle <mccardle.john@gmail.com>
    
    * Two more bios added
    
    * Two bio pics
    
    * Lee's bio
    
    Co-authored-by: John McCardle <mccardle.john@gmail.com>
    
    * Lee + Rob bio photos
    
    * Add bio entries
    
    * Whoops, fix Rob's filename
    
    * Photo of Jason
    
    * fixed brs logo issue
    
    ---------
    
    Co-authored-by: Aaron Storey <astoreyai@gmail.com>
    Co-authored-by: Andy Ponce <andy.ponce@outlook.com>
    Co-authored-by: Roberitonium <robert.evans.turner@gmail.com>
    Co-authored-by: Jason Jain <starlightjason2@gmail.com>
    Co-authored-by: Lee Gammon <scoutmurphy25@gmail.com>

--- frontend/pages/about-us.py
+++ frontend/pages/about-us.py
@@ -2,47 +2,60 @@ import dash
 from dash import html, dcc, callback, Input, Output
 from components.profile import Profile
 import dash_bootstrap_components as dbc
+import os
+
 dash.register_page(__name__)
 
 styles = {"padding": "20px"}
-description_placeholder = \
-"""
+description_placeholder = """
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus tempus vestibulum. 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
 Maecenas ante felis, sagittis ac convallis non, congue sit amet libero. Fusce ac accumsan augue. 
 Praesent quis ante est. Duis tristique porta magna, non mattis quam efficitur in. 
+
 Ut ac orci sed elit condimentum cursus. Fusce consequat purus in tincidunt volutpat. 
 Duis iaculis, odio vel pharetra maximus, ipsum neque rhoncus sapien, eget dictum purus eros a nibh. 
 Nunc nibh libero, iaculis sit amet vehicula in, auctor nec ante. Mauris imperdiet sodales quam, at lacinia ante efficitur sed. 
 Curabitur malesuada pellentesque rutrum. 
+
 Proin auctor ligula sem, et tristique magna iaculis mollis. Nulla ac dui justo. 
 """
 image_placeholder = "https://via.placeholder.com/200"
-bespoke_robot_image = \
-                dbc.Col(
-                    [
-                    html.Img(
-                        src="/assets/brs_transparency.png",  # replace with your image URL
-                        alt="Profile Picture",
-                        style={"width": "300px", "height": "300px"}
-                    ),
-                    ],
-                    width={"size": 2, "offset": 5},
-                    className="text-center"
-                )
-andy_profile = Profile("Andy Ponce", description_placeholder, "/assets/AndyPhoto.JPG")
+bespoke_robot_image = dbc.Col(
+    [
+        html.Img(
+            src="/assets/images/brs_transparency.png",
+            alt="Profile Picture",
+            style={"width": "300px", "height": "300px", "padding": "0px"},
+        ),
+    ],
+    width={"size": 2, "offset": 5},
+    className="text-center",
+)
+
+def bio_text(fn):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(dir_path, "..", fn), "r") as f:
+        return f.read()
+
+aaron_profile = Profile("Aaron W. Storey", bio_text("assets/aaron.txt"), "/assets/AaronPhoto.JPG")
+andy_profile = Profile("Andy Ponce", bio_text("assets/andy.txt"), "assets/AndyPhoto.JPG")
+john_profile = Profile("John P. McCardle III", bio_text("assets/john.txt"), "/assets/JohnPhoto.JPG")
+jason_profile = Profile("Jason Jain", bio_text("assets/jason.txt"), "/assets/JasonPhoto.JPG")
+lee_profile = Profile("Lee Gamon", bio_text("assets/lee.txt"), "/assets/LeePhoto.JPG")
+robert_profile = Profile("Robert Turner", bio_text("assets/rob.txt"), "/assets/RobPhoto.JPG")
 profile_placeholder = Profile("Your Name", description_placeholder, image_placeholder)
 
 layout = html.Div(
     [
         html.H1("About Us"),
         bespoke_robot_image,
-        profile_placeholder,
+        aaron_profile,
+        jason_profile,
         andy_profile,
-        profile_placeholder,
-        profile_placeholder,
-        profile_placeholder,
-        profile_placeholder,
+        robert_profile,
+        lee_profile,
+        john_profile,
     ],
     style=styles,
 )
